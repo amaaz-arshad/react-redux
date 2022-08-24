a25535b45799c3ff13740dd74a6313d7686b937f
Amaaz Arshad <amaaz992000@gmail.com>
2022-08-24 17:03:56 +0500

    first commit

---
+++ src/components/Profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+function Profile() {
+  const user = useSelector((state) => state.user.value);
+  const theme = useSelector((state) => state.theme.value);
+  return (
+    <div style={{ color: theme }}>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+    </div>
+  );
+}
+
+export default Profile;
